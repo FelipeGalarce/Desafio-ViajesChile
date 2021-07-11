32917ff29606baec495bddb481128e93231a5926
FelipeGalarce <felipe.gp26@gmail.com>
2021-07-11 13:26:32 -0400

    se agrega una segunda pagina para desplegar quienes somos en pantallas menores a 576px, ademas se agregan popovers a los iconos de la seccion quienes somos que desaparecen al hacer click en cualquiel lugar de la pagina, y tooltips en los iconos de redes sociales

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,14 @@ $(document).ready(function () {
     $(this).animate({ height : "100%", width: "100%" }, 'slow');
   });
 });
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+  trigger: 'focus'
+})
